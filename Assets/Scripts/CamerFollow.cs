e0bd8830ad2ec256abe70c2275b99ac6faff2f5a
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-03-22 17:19:35 +0100

    add my custom camera

---
+++ Assets/Scripts/CamerFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerFollow : MonoBehaviour
+{
+    public Transform player;
+    public float dampTime = 0.4f;
+    public float zDistance = -10f;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        cameraPos = new Vector3(player.position.x, player.position.y, zDistance);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+}
