bc4497d4b38389d0ed32f2712024436acf676975
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-03-25 21:00:25 +0100

    add material in player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class PlayerController : PhysicsObject {
 
+    [SerializeField]
     public float maxSpeed = 7;
+    [SerializeField]
     public float jumpTakeOffSpeed = 7;
+    [SerializeField]
     public float jumpPressedRememberTime = 0.2f;
+    [SerializeField]
+    public float groundedRememberTime = 0.2f;
+    [SerializeField]
+    [Range(0, 1)]
+    public float horizontalDampingBasic = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    public float horizontalDampingWhenStopping = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    public float horizontalDampingWhenTurning = 0.5f;
+    [SerializeField]
+    [Range(0, 1)]
+    public float cutJumpHeight = 0.5f;
+
 
     private SpriteRenderer spriteRenderer;
     // allows to jump few frames before to be grounded
     private float jumpPressedRemember = 0.0f;
+    // allow jump few frame after leaving the floor
+    private float groundedRemember = 0.0f;
     //private Animator animator;
+    private Weapon weapon;
+
 
     // Use this for initialization
     void Awake () 
     {
         spriteRenderer = GetComponent<SpriteRenderer> ();
         //animator = GetComponent<Animator> ();
+        weapon = transform.Find("WeaponPosition").Find("Weapon").GetComponent<Weapon>();
     }
 
     protected override void ComputeVelocity()
@@ -25,20 +48,50 @@ public class PlayerController : PhysicsObject {
         Vector2 move = Vector2.zero;
 
         move.x = Input.GetAxis ("Horizontal");
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
+        {
+            move.x *= Mathf.Pow(1f - horizontalDampingWhenStopping, Time.deltaTime * 10f);
+        }
+        else if (Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(move.x))
+        {
+            move.x *= Mathf.Pow(1f - horizontalDampingWhenTurning, Time.deltaTime * 10f);
+        }
+        else
+        {
+            move.x *= Mathf.Pow(1f - horizontalDampingBasic, Time.deltaTime * 10f);
+        }
+
+
+        groundedRemember = groundedRemember - Time.deltaTime;
+        if(grounded)
+        {
+            groundedRemember = groundedRememberTime;
+        }
 
         jumpPressedRemember = jumpPressedRemember - Time.deltaTime;
-        // attemps to jump
-        if ((jumpPressedRemember > 0.0f) && grounded) {
-            jumpPressedRemember = 0.0f;
-            velocity.y = jumpTakeOffSpeed;
-        // press jump
-        } else if (Input.GetButtonUp ("Jump"))
-        {
+
+        if(Input.GetButtonDown("Jump")) {
             jumpPressedRemember = jumpPressedRememberTime;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
             if (velocity.y > 0) {
-                velocity.y = velocity.y * 0.5f;
+                velocity.y = velocity.y * cutJumpHeight;
             }
         }
+
+        if ((jumpPressedRemember > 0.0f) && (groundedRemember > 0.0f)) {
+            jumpPressedRemember = 0.0f;
+            groundedRemember = 0.0f;
+            velocity.y = jumpTakeOffSpeed;
+        }
+
+        if(Input.GetButtonUp("Fire1")) {
+            Debug.Log("hello");
+            velocity.y = weapon.TriggerGun();
+        }
+
 
         bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
         if (flipSprite) 
@@ -48,7 +101,6 @@ public class PlayerController : PhysicsObject {
 
         //animator.SetBool ("grounded", grounded);
         //animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
-
         targetVelocity = move * maxSpeed;
     }
 }
