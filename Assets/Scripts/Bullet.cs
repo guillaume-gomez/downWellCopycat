d1e2ade5f4f1ea792eef186b2c0539778e7dd54c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-04-13 21:39:04 +0200

    move responsabiliy in who can destroy who

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,7 +7,25 @@ public class Bullet : MonoBehaviour
     void Start()
     {   
         transform.Rotate(new Vector3(0,0,90));
-        // if body does not collide, it destros itself after 2 secondes
+        // if body does not collide, it destroy itself after 2 secondes
         Destroy(gameObject, 2);
     }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        BreakableBloc bloc = collision.collider.GetComponent<BreakableBloc>();
+        if(bloc) {
+            bloc.DamageBloc(1);
+            return;
+        }
+
+        EnemyBase enemy = collision.collider.GetComponent<EnemyBase>();
+        if(enemy) {
+            enemy.Hurt();
+            return;
+        }
+    }
+
 }
--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -4,23 +4,6 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        // let the player check this by himself
-        // if (collision.gameObject.tag == "Player")
-        // {
-        //     PlayerController player = collision.collider.GetComponent<PlayerController>();
-        //     if(!player) {
-        //         return;
-        //     }
-        //     player.Hurt();
-        // }
-        if(collision.gameObject.tag == "Bullet")
-        {
-          Hurt();
-        }
-    }
-
     public void Hurt() {
       Destroy(this.gameObject);
     }
