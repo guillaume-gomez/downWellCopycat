feadc796753757eb5ce6823015f90f5197c2a453
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-04-21 18:52:08 +0200

    create extruder to bloc after level generation. yup kinda quick and dirty :p

---
+++ Assets/Scripts/Extruder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this component is called to remove bloc generated during the level
+public class Extruder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+        Destroy(gameObject);
+    }
+}
