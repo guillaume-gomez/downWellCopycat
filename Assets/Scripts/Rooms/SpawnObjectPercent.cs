dff7b2f25c6fce67d26636cdd599aaefb42257f9
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-05-11 22:28:15 +0200

    create random object spawner

---
+++ Assets/Scripts/Rooms/SpawnObjectPercent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectPercent : SpawnObject
+{
+    [Range(0.0f, 1.0f)]
+    public float percent = 1.0f;
+
+    public new void Init()
+    {
+        if(Random.Range(0.0f, 1.0f) <= percent) {
+            base.Init();
+        }
+    }
+
+}
