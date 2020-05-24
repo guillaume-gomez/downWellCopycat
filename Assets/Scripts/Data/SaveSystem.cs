c18e77866d93e7697eb7c84263b16c768ca25b96
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-05-24 22:15:33 +0200

    add character stats

--- Assets/Scripts/Data/SaveSystem.cs
+++ Assets/Scripts/Data/SaveSystem.cs
@@ -10,7 +10,7 @@ public static class SaveSystem
         string path = Application.persistentDataPath + "/game.dat";
         FileStream stream = new FileStream(path, FileMode.Create);
         
-        formatter.Serialize(stream, GameManager.instance.Gamedata);
+        formatter.Serialize(stream, GameManager.instance.GameData);
         stream.Close();
     }
 
@@ -30,6 +30,35 @@ public static class SaveSystem
         {
             Debug.LogError("Save file not found in " + path);
             return new GameData(-1,-1, -1);
+        }
+    }
+
+    public static void SaveCharacterStat()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/characterStat.dat";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        
+        formatter.Serialize(stream, GameManager.instance.CharacterStats);
+        stream.Close();
+    }
+
+    public static CharacterStats LoadCharacterStat()
+    {
+        string path = Application.persistentDataPath + "/characterStat.dat";
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            CharacterStats data = formatter.Deserialize(stream) as CharacterStats;
+            stream.Close();
+
+            return data;
+        } else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return new CharacterStats();
         }
     }
 }
