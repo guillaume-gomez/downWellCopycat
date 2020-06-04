c556a4a144b8881d41de315fbef6d9778cff4097
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-06-04 19:55:23 +0200

    add leveling data

---
+++ Assets/Scripts/Leveling/LevelSystem.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class LevelSystem
+{
+
+    public event EventHandler OnExperienceChanged;
+    public event EventHandler OnLevelChanged;
+
+    private int level;
+    private int experience;
+    private int experienceToNextLevel;
+
+    public LevelSystem()
+    {
+        level = 0;
+        experience = 0;
+        experienceToNextLevel = 100;
+    }
+
+    public void AddExperience(int amount)
+    {
+        experience += amount;
+        if(experience >= experienceToNextLevel)
+        {
+            level++;
+            experience -= experienceToNextLevel;
+            if(OnLevelChanged != null)
+            {
+                OnExperienceChanged(this, EventArgs.Empty);
+            }
+        }
+        if(OnExperienceChanged != null)
+        {
+            OnExperienceChanged(this, EventArgs.Empty);
+        }
+    }
+
+    public int GetLevelNumber()
+    {
+        return level;
+    }
+
+}
