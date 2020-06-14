b848e7824be73d35a24248a8956acd862c5c252a
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-06-14 23:05:03 +0200

    redirect to market

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Market()
+    {
+        SceneManager.LoadScene(3);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit");
