ea1ee7c0708998bfe6a0032ba07bbbb75a35527b
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-06-21 19:05:37 +0200

    remove start and update call

--- Assets/Scripts/Rooms/bloc.cs
+++ Assets/Scripts/Rooms/bloc.cs
@@ -4,16 +4,4 @@ using UnityEngine;
 
 public class bloc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        //Debug.Log("stated bloc");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
