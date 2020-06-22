089a36736cd1c0c5715978dcd60693239210b977
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-06-22 19:12:56 +0200

    add size and color in enemy

--- Assets/Scripts/NPC/EnemyBase.cs
+++ Assets/Scripts/NPC/EnemyBase.cs
@@ -9,18 +9,25 @@ static class EnemyConstants
         new Color( 0.0f, 0.0f, 0.65f, 1.0f ),
         new Color( 0.0f, 0.0f, 0.49f, 1.0f ),
     };
+
+    public static readonly Vector3[] Size = {
+        new Vector3( 1.0f, 1.0f, 1.0f  ),
+        new Vector3( 2.0f, 2.0f, 2.0f ),
+        new Vector3( 3.0f, 3.0f, 3.0f ),
+    };
 }
 
 
 public class EnemyBase : MonoBehaviour
 {
-    private SpriteRenderer sprite;
+    public Transform target;
     public int enemyScore = 0;
     [Range(1, 4)]
     public float life = 1;
+    public GameObject coin;
     protected Vector3 slotSize;
     protected Vector3 slotPosition;
-    public Transform target;
+    private SpriteRenderer sprite;
 
     public float Life {
         get => life;
@@ -32,6 +39,7 @@ public class EnemyBase : MonoBehaviour
     {
         sprite = GetComponent<SpriteRenderer>();
         computeColor();
+        computeSize();
     }
 
     protected void Start()
@@ -50,8 +58,9 @@ public class EnemyBase : MonoBehaviour
         {
             Destroy(this.gameObject);
             LevelManager.instance.AddScore(enemyScore);
-            LevelManager.instance.IncCombo();
             LevelManager.instance.IncEnemyKill();
+            LevelManager.instance.IncCombo();
+            Instantiate(coin, transform.position, transform.rotation);
         }
     }
 
@@ -88,5 +97,24 @@ public class EnemyBase : MonoBehaviour
             break;
         }
         sprite.color = color;
+    }
+
+    private void computeSize()
+    {
+        Vector3 size = new Vector3();
+        switch(life) {
+            case 1:
+                transform.localScale = EnemyConstants.Size[0];
+            break;
+            case 2:
+                transform.localScale = EnemyConstants.Size[1];
+            break;
+            case 3:
+                transform.localScale = EnemyConstants.Size[2];
+            break;
+            default:
+                transform.localScale = EnemyConstants.Size[2];
+            break;
+        }
     }
 }
