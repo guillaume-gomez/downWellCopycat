4a490534c6b888a43e6759b186e0631dc6a59037
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-06-25 10:38:06 +0200

    add different percentage of generation

--- Assets/Scripts/Rooms/RoomGen.cs
+++ Assets/Scripts/Rooms/RoomGen.cs
@@ -8,7 +8,9 @@ public class RoomGen : MonoBehaviour
     public SpawnObject[] rights;
     public SpawnObject[] centers;
     [Range(0,1.0f)]
-    public float percentage = 1.0f;
+    public float percentageCenter = 1.0f;
+    [Range(0,1.0f)]
+    public float percentageSide = 1.0f;
 
 
     // Start is called before the first frame update
@@ -16,7 +18,7 @@ public class RoomGen : MonoBehaviour
     {
         for(int i = 0; i < lefts.Length; i++)
         {
-            if(lefts[i].gameObject.activeSelf && Random.Range(0.0f, 1.0f) <= percentage)
+            if(lefts[i].gameObject.activeSelf && Random.Range(0.0f, 1.0f) <= percentageSide)
             {
 
                 lefts[i].Init();
@@ -25,7 +27,7 @@ public class RoomGen : MonoBehaviour
 
         for(int i = 0; i < rights.Length; i++)
         {
-            if(rights[i].gameObject.activeSelf && Random.Range(0.0f, 1.0f) <= percentage)
+            if(rights[i].gameObject.activeSelf && Random.Range(0.0f, 1.0f) <= percentageSide)
             {
                 rights[i].Init();
             }
@@ -33,7 +35,7 @@ public class RoomGen : MonoBehaviour
 
         for(int i = 0; i < centers.Length; i++)
         {
-            if(centers[i].gameObject.activeSelf && Random.Range(0.0f, 1.0f) <= percentage)
+            if(centers[i].gameObject.activeSelf && Random.Range(0.0f, 1.0f) <= percentageCenter)
             {
                 centers[i].Init();
             }
