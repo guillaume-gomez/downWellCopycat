84a34b6b530b954ea82ecadb8c6b4bf06e8f65d2
guillaume-gomez <guillaume.gomez846@gmail.com>
2020-07-03 00:32:49 +0200

    add new character stats

--- Assets/Scripts/character/CharacterStats.cs
+++ Assets/Scripts/character/CharacterStats.cs
@@ -6,12 +6,16 @@ public class CharacterStats
 {
     public CharacterStat life;
     public CharacterStat weaponAbilities;
+    public CharacterStat jumpTakeOffSpeed;
+    public CharacterStat maxSpeed;
 
     public CharacterStats()
     {
         // default value of life
         life = new CharacterStat(4);
         weaponAbilities = new CharacterStat(12);
+        jumpTakeOffSpeed = new CharacterStat(0.0f);
+        maxSpeed = new CharacterStat(0.0f);
     }
 
     public void Reset()
@@ -19,5 +23,7 @@ public class CharacterStats
 
         life.RemoveAllNotifier();
         weaponAbilities.RemoveAllNotifier();
+        jumpTakeOffSpeed.RemoveAllNotifier();
+        maxSpeed.RemoveAllNotifier();
     }
 }
