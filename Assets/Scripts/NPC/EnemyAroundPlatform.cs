6c526dbbc032204f3b89dab191be29f40fe49dbc
guillaume-gomez <guillaume.gomez846@gmail.com>
2020-07-30 22:53:10 +0200

    update algorithm to detect blocs

--- Assets/Scripts/NPC/EnemyAroundPlatform.cs
+++ Assets/Scripts/NPC/EnemyAroundPlatform.cs
@@ -6,10 +6,16 @@ public class EnemyAroundPlatform : EnemyBase
 {
   public float speed;
   public Transform[] groundDetections;
-  int layerMastk;
+  private int layerMastk;
+  protected Rigidbody2D rb2d;
 
   private float distance = 1.0f;
   private bool movingRight = true;
+
+  protected void OnEnable()
+  {
+      rb2d = GetComponent<Rigidbody2D>();
+  }
 
   protected void Start()
   {
@@ -39,16 +45,27 @@ public class EnemyAroundPlatform : EnemyBase
       return;
     }
 
-    Vector2 direction = movingRight ? Vector2.right : - Vector2.right;
-    transform.Translate(direction * speed * Time.deltaTime);
+    Vector3 direction = movingRight ? transform.right : - transform.right;
+    rb2d.position = rb2d.position + (new Vector2(direction.x, direction.y) * speed * Time.deltaTime);
     if(!onPlatform())
     {
         //Debug.Break();
         transform.eulerAngles += new Vector3(0.0f, 0.0f, -90f);
+        for(int i = 0; i < groundDetections.Length; ++i)
+        {
+          RaycastHit2D groundInfo = Physics2D.Raycast(groundDetections[i].position, -transform.up, distance, layerMastk);
+          Debug.DrawLine(groundDetections[i].position, groundDetections[i].position - (transform.up * distance), Color.red,100);
+          if (groundInfo.collider && (groundInfo.collider.tag == "Bloc" || groundInfo.collider.tag == "BreakableBloc") )
+          {
+            //return true;
+            transform.position += -groundInfo.distance * transform.up;
+            break;
+          }
+        }
+        //Debug.DrawLine(transform.position, transform.position - (transform.up * distance), Color.red,100);
+        //Debug.Break();
     } else {
       // adjust position due to rotation overlap
-      RaycastHit2D groundInfo = Physics2D.Raycast(groundDetections[0].position, -transform.up, distance, layerMastk);
-      transform.position += - groundInfo.distance * transform.up;
     }
   }
 
