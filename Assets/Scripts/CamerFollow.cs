88511a7f05215ec01791b0c6be9793bb8087ccc0
guillaume-gomez <guillaume.gomez846@gmail.com>
2020-08-15 00:16:15 +0200

    add shake when the player is hurt

--- Assets/Scripts/CamerFollow.cs
+++ Assets/Scripts/CamerFollow.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class CamerFollow : MonoBehaviour
 {
-    public Transform player;
+    public PlayerController player;
     public float dampTime = 0.1f;
     public Vector3 offset;
     private Vector3 velocity = Vector3.zero;
@@ -17,6 +16,8 @@ public class CamerFollow : MonoBehaviour
         {
             LevelManager.instance.OnWin += OnUnFollow;
         }
+        player.OnLifeChanged += OnPlayerHurt;
+
     }
 
     void Update()
@@ -26,7 +27,7 @@ public class CamerFollow : MonoBehaviour
             return;
         }
 
-        Vector3 cameraPos = new Vector3(player.position.x, player.position.y, transform.position.z) + offset;
+        Vector3 cameraPos = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z) + offset;
         transform.position = Vector3.SmoothDamp(transform.position, cameraPos, ref velocity, dampTime);
     }
 
@@ -39,6 +40,28 @@ public class CamerFollow : MonoBehaviour
     private void OnUnFollow(object sender, System.EventArgs e)
     {
         Unfollow();
+    }
+
+    void OnPlayerHurt(object sender, OnLifeChangedEventArgs e)
+    {
+        StartCoroutine(Shake(0.05f, 0.4f));
+    }
+
+    IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+        float elapsed = 0.0f;
+
+        while(elapsed < duration)
+        {
+            float x = Random.Range(-1.0f, 1.0f) * magnitude;
+            float y = Random.Range(-1.0f, 1.0f) * magnitude;
+
+            transform.localPosition = new Vector3(originalPos.x + x, originalPos.y + y, originalPos.z);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = originalPos;
     }
 
 
