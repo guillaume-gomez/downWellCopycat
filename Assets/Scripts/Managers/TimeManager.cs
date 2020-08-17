425f06cc493ab7e70467998f31553d6719170b24
guillaume-gomez <guillaume.gomez846@gmail.com>
2020-08-18 00:17:37 +0200

    improve perf by using a corountine

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
 
 public class TimeManager : MonoBehaviour {
 
@@ -8,23 +9,28 @@ public class TimeManager : MonoBehaviour {
 
   private static float frameTime = 0.02f;//1.0f/50.f;
 
-  void Update()
-  {
-    if(PauseMenu.GameIsPaused || LevelManager.PauseGame) {
-      return;
-    }
-    Time.timeScale += (1.0f/ slowdownLength)  *  Time.unscaledDeltaTime;
-    Time.timeScale = Mathf.Clamp(Time.timeScale, 0.0f, 1.0f);
-    if (Time.timeScale == 1.0f) {
-      Time.fixedDeltaTime = Time.deltaTime;
-    }
-
-  }
-
-
   public void DoSlowMotion()
   {
     Time.timeScale = slowdownFactor;
     Time.fixedDeltaTime = Time.timeScale * frameTime;
+    StartCoroutine("GetBack");
+  }
+
+  IEnumerator GetBack()
+  {
+    while(Time.timeScale < 1.0f)
+    {
+      if(PauseMenu.GameIsPaused || LevelManager.PauseGame) {
+        yield return null;
+      }
+      Time.timeScale += (1.0f/ slowdownLength)  *  Time.unscaledDeltaTime;
+      Time.timeScale = Mathf.Clamp(Time.timeScale, 0.0f, 1.0f);
+      yield return null;
+    }
+
+    if (Time.timeScale == 1.0f)
+    {
+      Time.fixedDeltaTime = Time.deltaTime;
+    }
   }
 }
