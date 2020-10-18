7739a2bd282391e3aae5b65bb07c245286df38ff
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-10-18 22:44:59 +0200

    conditionally destroy items

---
+++ Assets/Scripts/Rooms/BreakableBlocShop.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class BreakableBlocShop : MonoBehaviour
+{
+    public SpawnObject spawner;
+    private float priceToBeDestroyed;
+
+    void Start()
+    {
+        spawner.Init();
+        // We assume spawner has script PickupBoughtItem
+        PickupBoughtItem script = spawner.gameObject.transform.GetChild(0).GetComponent<PickupBoughtItem>();
+        Debug.Log(priceToBeDestroyed);
+        priceToBeDestroyed = script.price;
+        Debug.Log(priceToBeDestroyed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(priceToBeDestroyed > 10) {
+            if(collision.collider.CompareTag("Bullet"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
