f21adbdd59284a2aba4fa8f59164d65192f7c1bd
Guillaume Gomez <guillaume.gomez846@gmail.com>
2020-12-03 22:09:34 +0100

    change cave to fit the new level

--- Assets/Scripts/Rooms/RoomGenCave.cs
+++ Assets/Scripts/Rooms/RoomGenCave.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class RoomGenCave : MonoBehaviour
 {
     public GameObject[] possibleCavePosition;
-    // Start is called before the first frame update
     void Start()
     {
         int rand = Random.Range(0, possibleCavePosition.Length);
