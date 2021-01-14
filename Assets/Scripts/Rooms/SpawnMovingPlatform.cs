a6eb9ad0f8d14393e0aa98d600ce7d286e73a54a
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-01-14 20:48:32 +0100

    add new type of path

--- Assets/Scripts/Rooms/SpawnMovingPlatform.cs
+++ Assets/Scripts/Rooms/SpawnMovingPlatform.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PathType {
+Triangle,
+Square,
+}
+
 public class SpawnMovingPlatform : MonoBehaviour
 {
+  public PathType pathType;
   public GameObject prefabMovingPlatform;
 
   void Start()
   {
     GameObject instance = (GameObject) Instantiate(prefabMovingPlatform, transform.position, transform.rotation);
     instance.transform.parent = transform;
-    TrianglePath(instance);
+    switch(pathType)
+    {
+      case PathType.Triangle:
+        TrianglePath(instance);
+      break;
+      case PathType.Square:
+        SquarePath(instance);
+      break;
+    }
   }
 
   void TrianglePath(GameObject instance)
@@ -33,6 +47,35 @@ public class SpawnMovingPlatform : MonoBehaviour
     spots.Add(dot1);
     spots.Add(dot2);
     spots.Add(dot3);
+    script.spots = spots.ToArray();
+
+    platformPivot.transform.position = dot1.transform.position;
+  }
+
+  void SquarePath(GameObject instance)
+  {
+    GameObject platformPivot = instance.transform.Find("PlatformPivot").gameObject;
+    MovingPlatform script = platformPivot.GetComponent<MovingPlatform>();
+
+    Transform dots = instance.transform.Find("Dots");
+    GameObject dot1 = new GameObject("dot1");
+    dot1.transform.position = new Vector3(-5.0f, 0.0f, 0.0f);
+    dot1.transform.parent = dots.transform;
+    GameObject dot2 = new GameObject("dot2");
+    dot2.transform.position = new Vector3(5.0f, 0.0f, 0.0f);
+    dot2.transform.parent = dots.transform;
+    GameObject dot3 = new GameObject("dot3");
+    dot3.transform.position = new Vector3(5.0f, 5.0f, 0.0f);
+    dot3.transform.parent = dots.transform;
+    GameObject dot4 = new GameObject("dot3");
+    dot4.transform.position = new Vector3(-5.0f, 5.0f, 0.0f);
+    dot4.transform.parent = dots.transform;
+
+    List<GameObject> spots = new List<GameObject>();
+    spots.Add(dot1);
+    spots.Add(dot2);
+    spots.Add(dot3);
+    spots.Add(dot4);
     script.spots = spots.ToArray();
 
     platformPivot.transform.position = dot1.transform.position;
