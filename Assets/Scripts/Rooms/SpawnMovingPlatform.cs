a60943b273dd8fc0f458ee93c7a6654d80da74f6
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-01-17 00:26:34 +0100

    add new variable to control the size of the path

--- Assets/Scripts/Rooms/SpawnMovingPlatform.cs
+++ Assets/Scripts/Rooms/SpawnMovingPlatform.cs
@@ -11,6 +11,7 @@ public class SpawnMovingPlatform : MonoBehaviour
 {
   public PathType pathType;
   public GameObject prefabMovingPlatform;
+  public Vector2 size;
 
   void Start()
   {
@@ -32,15 +33,18 @@ public class SpawnMovingPlatform : MonoBehaviour
     GameObject platformPivot = instance.transform.Find("PlatformPivot").gameObject;
     MovingPlatform script = platformPivot.GetComponent<MovingPlatform>();
 
+    float middleX = size.x / 2.0f;
+    float middleY = size.y / 2.0f;
+
     Transform dots = instance.transform.Find("Dots");
     GameObject dot1 = new GameObject("dot1");
-    dot1.transform.position = new Vector3(-5.0f, 0.0f, 0.0f);
+    dot1.transform.position = new Vector3(-middleX, -middleY, 0.0f);
     dot1.transform.parent = dots.transform;
     GameObject dot2 = new GameObject("dot2");
-    dot2.transform.position = new Vector3(5.0f, 0.0f, 0.0f);
+    dot2.transform.position = new Vector3(middleX, -middleY, 0.0f);
     dot2.transform.parent = dots.transform;
     GameObject dot3 = new GameObject("dot3");
-    dot3.transform.position = new Vector3(0.0f, -10.0f, 0.0f);
+    dot3.transform.position = new Vector3(0.0f, middleY, 0.0f);
     dot3.transform.parent = dots.transform;
 
     List<GameObject> spots = new List<GameObject>();
@@ -57,18 +61,21 @@ public class SpawnMovingPlatform : MonoBehaviour
     GameObject platformPivot = instance.transform.Find("PlatformPivot").gameObject;
     MovingPlatform script = platformPivot.GetComponent<MovingPlatform>();
 
+    float middleX = size.x / 2.0f;
+    float middleY = size.y / 2.0f;
+
     Transform dots = instance.transform.Find("Dots");
     GameObject dot1 = new GameObject("dot1");
-    dot1.transform.position = new Vector3(-5.0f, 0.0f, 0.0f);
+    dot1.transform.position = new Vector3(-middleX, -middleY, 0.0f);
     dot1.transform.parent = dots.transform;
     GameObject dot2 = new GameObject("dot2");
-    dot2.transform.position = new Vector3(5.0f, 0.0f, 0.0f);
+    dot2.transform.position = new Vector3(middleX, -middleY, 0.0f);
     dot2.transform.parent = dots.transform;
     GameObject dot3 = new GameObject("dot3");
-    dot3.transform.position = new Vector3(5.0f, 5.0f, 0.0f);
+    dot3.transform.position = new Vector3(middleX, middleY, 0.0f);
     dot3.transform.parent = dots.transform;
     GameObject dot4 = new GameObject("dot3");
-    dot4.transform.position = new Vector3(-5.0f, 5.0f, 0.0f);
+    dot4.transform.position = new Vector3(-middleX, middleY, 0.0f);
     dot4.transform.parent = dots.transform;
 
     List<GameObject> spots = new List<GameObject>();
