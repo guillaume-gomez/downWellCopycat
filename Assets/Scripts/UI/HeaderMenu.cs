3a98e8ff2eb9a052e545d654910b7fb074806f0d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-09-04 14:30:14 +0200

    add new value in EventMoneyArgs + display price paid + fix baground color

--- Assets/Scripts/UI/HeaderMenu.cs
+++ Assets/Scripts/UI/HeaderMenu.cs
@@ -8,17 +8,26 @@ public class HeaderMenu : MonoBehaviour
 {
     public TextMeshProUGUI score;
     public TextMeshProUGUI money;
-
+    public TextMeshProUGUI pricePaid;
     void Start()
     {
       MarketManager.instance.OnMoneyChange += OnMoneyChange;
       score.text = "Score: " + MarketManager.instance.Score.ToString("F2");
       money.text = "Money: " + MarketManager.instance.Money.ToString("F2");
+      pricePaid.text = "";
     }
 
    private void OnMoneyChange(object sender, OnMoneyChangedEventArgs e)
    {
       money.text = "Money: " + e.money.ToString("F2");
+      StartCoroutine(FlashPricePaid(e.pricePaid));
+   }
+
+   IEnumerator FlashPricePaid(float pricePaidEvent) {
+      pricePaid.text = "- " + pricePaidEvent.ToString("F2");
+      yield return new WaitForSeconds(1f);
+      pricePaid.text = "";
+
    }
 
 }
